bd21cfe3e290caf6cfc92953a970ea2266e93d05
libmanuk <eweig@email.uky.edu>
2014-06-19 16:01:44 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -1,6 +1,6 @@
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 <div id="primary">
-    <!--<h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>-->
+    
 
     <!-- Items metadata -->
     <div id="item-metadata">
