f4f78ca828fddfcc99f1334f5ee3b88ac346d28a
libmanuk <eweig@email.uky.edu>
2015-02-10 07:14:11 -0500

    Update show.php

--- items/show.php
+++ items/show.php
@@ -22,9 +22,9 @@
     </div>
 
     <!--<h3><?php echo __('Files'); ?></h3>-->
-    <div id="item-images">
+    <!--<div id="item-images">
          <?php echo files_for_item(); ?>
-    </div>
+    </div>-->
 
    <?php if(metadata('item','Collection Name')): ?>
       <div id="collection" class="element">
@@ -42,10 +42,10 @@
     <?php endif;?>
 
     <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
+    <!--<div id="item-citation" class="element">
         <h3><?php echo __('Citation'); ?></h3>
         <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
-    </div>
+    </div>-->
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
