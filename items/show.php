e3167cbd5667663b446b6914e52fae289220ec0f
libmanuk <eweig@email.uky.edu>
2015-02-11 05:34:08 -0500

    Update show.php

--- items/show.php
+++ items/show.php
@@ -40,11 +40,18 @@
         <div class="element-text"><a href="<?php echo metadata('item', array('Dublin Core', 'Relation')); ?>" target="_blank"><img src="/images/icon_play2.png"/></a><br/><br/></div>
 <?php endif; ?>
 
-<?php if ($lcsubjects = metadata('item', array('Item Type Metadata', 'Interview LC Subjects'), array('delimiter'=>', '))): ?>
+<?php if ($insubjects = metadata('item', array('Item Type Metadata', 'Interview LC Subjects'), array('delimiter'=>', '))): ?>
 <div class="item-description-static">
 <h3>Interview LC Subjects:</h3>
-<?php echo $lcsubjects; ?>
-</div>
+<?php echo $insubjects; ?>
+<br/></div>
+<?php endif; ?>
+
+<?php if ($inkeywords = metadata('item', array('Item Type Metadata', 'Interview Keywords'), array('delimiter'=>', '))): ?>
+<div class="item-description-static">
+<h3>Interview Keywords:</h3>
+<?php echo $inkeywords; ?>
+<br/></div>
 <?php endif; ?>
 
         <?php echo all_element_texts('item'); ?>
