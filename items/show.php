f74c7b5368781873a04519812b47cec8d350212d
libmanuk <eweig@email.uky.edu>
2015-03-17 15:11:43 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -221,7 +221,7 @@
             </div>
             <div id="view9">
 </div>
- </div> 
+ </div> </div>
     <ul class="item-pagination navigation">
         <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
         <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
