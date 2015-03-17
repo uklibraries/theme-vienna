b49a39880a2b0dfbfe31d45bec8d17b2b5fc6c6c
libmanuk <eweig@email.uky.edu>
2015-03-17 14:51:37 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -39,7 +39,7 @@
     <div id="item-metadata">
     <!--<?php echo all_element_texts('item'); ?>-->
     
-    <div style="width: 500px; margin: 0 auto; padding: 120px 0 40px;">
+    <div style="width: 100%; margin: 0 auto; padding: 120px 0 40px;">
         <ul class="tabs" data-persist="true">
             <li><a href="#view1">General</a></li>
             <li><a href="#view2">Description</a></li>
@@ -233,7 +233,7 @@
         </div>-->
 
 </div>
-
+</div>
  </div> 
     <ul class="item-pagination navigation">
         <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
