c6fa7f8b59534fa505ffcc3910f8ad101b826806
libmanuk <eweig@email.uky.edu>
2015-03-17 13:21:43 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -196,7 +196,10 @@
 </div>
 <?php endif; ?>
 
-
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
 
  </div> 
 
@@ -231,11 +234,8 @@
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
-    <ul class="item-pagination navigation">
-        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
+
 
 </div> <!-- End of Primary. -->
-</div>
+
  <?php echo foot(); ?>
