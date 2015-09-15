714b0b9f4547189506c30330b42fd44bf7c01af9
libmanuk <eweig@email.uky.edu>
2015-09-15 15:01:04 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -199,21 +199,21 @@
         <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interview Summary: </h4></td><td style="width:300px;text-align:left;"><?php echo metadata('item', array('Item Type Metadata', 'Interview Summary')); ?></td></tr></table></div>
 <?php endif; ?>
 
-<div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interviewee First Name, Middle, Last: </h4></td>
+<div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interviewee First Name, Middle, Last: </h4></td><td>
 <?php if(metadata('item', array('Item Type Metadata', 'Interviewee First Name'))): ?>
-<td style="width:300px;text-align:left;"><?php echo metadata('item', array('Item Type Metadata', 'Interviewee First Name')); ?></td><?php endif; ?>
+<?php echo metadata('item', array('Item Type Metadata', 'Interviewee First Name')); ?>, <?php endif; ?>
 <?php if(metadata('item', array('Item Type Metadata', 'Interviewee Middle Name'))): ?>
-<td style="width:300px;text-align:left;"><?php echo metadata('item', array('Item Type Metadata', 'Interviewee Middle Name')); ?></td>
+<?php echo metadata('item', array('Item Type Metadata', 'Interviewee Middle Name')); ?>, 
 <?php endif; ?>
 <?php if(metadata('item', array('Item Type Metadata', 'Interviewee Last Name'))): ?>
-<td style="width:300px;text-align:left;"><?php echo metadata('item', array('Item Type Metadata', 'Interviewee Last Name')); ?></td>
+<?php echo metadata('item', array('Item Type Metadata', 'Interviewee Last Name')); ?>
 <?php endif; ?>
-</tr></table></div>
+</td></tr></table></div>
 
 <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interviewer First Name, Middle, Last: </h4></td><td>
 <?php if(metadata('item', array('Item Type Metadata', 'Interviewer First Name'))): ?>
 <?php echo metadata('item', array('Item Type Metadata', 'Interviewer First Name')); ?>, <?php endif; ?>
-<?php if(metadata('item', array('Item Type Metadata', 'Interviewer Middle Name'))): ?>
+<?php if(metadata('item', array('Item Type Metadata', 'Interviewee Middle Name'))): ?>
 <?php echo metadata('item', array('Item Type Metadata', 'Interviewee Middle Name')); ?>, 
 <?php endif; ?>
 <?php if(metadata('item', array('Item Type Metadata', 'Interviewer Last Name'))): ?>
