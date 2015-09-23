c246db3325bd0ed2464eda6ed685f20519ef5121
libmanuk <eweig@email.uky.edu>
2015-09-23 09:56:30 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -416,7 +416,7 @@ if ('interviews' === $type): ?>
 <?php 
 $type = $item->getItemType()->name;
 if ('interviews' === $type): ?>
-<?php if ($intech = metadata('item', array('Item Type Metadata', 'Interview Technical Overview'), array('delimiter'=>'<br/><br/> '))): ?>
+<?php if ($intech = metadata('item', array('Item Type Metadata', 'Interview Technical Overview'), array('delimiter'=>'<br/><br/> '), array('no_filter' => true))): ?>
 <table style="width: 100%;"><tr><td style="width:100px;text-align:left;"><h4>Interview Technical Overview:</h4></td><td style="width:300px;text-align:left;">
 <div class="item-description-static">
 <div id="test">
