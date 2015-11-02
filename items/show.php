249b599dca9ca7533af27242d816fa73eb9cb1e2
libmanuk <eweig@email.uky.edu>
2015-11-02 10:06:38 -0500

    Update show.php

--- items/show.php
+++ items/show.php
@@ -619,10 +619,10 @@ if ('series' === $type): ?>
 <?php if(metadata('item', array('Item Type Metadata', 'Series ARK Identifier'))): ?>
         <p>Is this item online?  <a href="https://oralhistory.uky.edu/catalog/<?php echo metadata('item', array('Item Type Metadata', 'Series ARK Identifier')); ?>" data-lity>https://oralhistory.uky.edu/catalog/<?php echo metadata('item', array('Item Type Metadata', 'Series ARK Identifier')); ?></a></p>
 <?php endif; ?>
-<form method="post" action=""> 
+<!--<form method="post" action=""> 
 <input type="text" name="name" value="<?php echo metadata('item', array('Item Type Metadata', 'Interview ARK Identifier')); ?>" readonly="readonly" style="border:none;display:none;">
 <input type="submit" name="delete" value="Delete Item from SPOKEdb Public"> 
-</form>
+</form>-->
 <br/>
 <p>Documents:</p>
 <p><A href="https://docs.google.com/spreadsheets/d/1-CkmS0a1s_ZUOFDzgRMeNRLwHZyikG712YRYFJAoLCo/edit#gid=0" data-lity>SPOKEdb Technical Metadata Specification</a><br/>
