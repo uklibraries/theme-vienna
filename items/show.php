6f7b9a941018664fd882f188b6e433f3c86949e8
libmanuk <eweig@email.uky.edu>
2015-12-04 14:51:37 -0500

    Update show.php

--- items/show.php
+++ items/show.php
@@ -606,7 +606,7 @@ if ('collections' === $type): ?>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#60;p&#62;&#60;/p&#62;<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#60;/acqinfo&#62;<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#60;controlaccess&#62;<br/>
-<?php if ($clcsubject = metadata('item', array('Item Type Metadata', 'Collection LC Subject'), array('delimiter'=>', '))): ?><br/>
+<?php if ($clcsubject = metadata('item', array('Item Type Metadata', 'Collection LC Subject'), array('delimiter'=>'&#60;/subject&#62;&#60;subject source="lcsh"&#62;'))): ?><br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#60;subject source="lcsh"&#62;<?php echo $clcsubject; ?>&#60;/subject&#62;<br/>
 <?php endif; ?><br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#60;/controlaccess&#62;<br/>
