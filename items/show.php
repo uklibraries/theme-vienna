328c5576cf60d3c492d0ae37405aba56f78c858f
libmanuk <eweig@email.uky.edu>
2016-05-01 16:42:16 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -199,10 +199,11 @@ if ('interviews' === $type): ?>
         <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Link to Interview Media File: </h4></td><td style="width:300px;text-align:left;"><a href="http://athena.uky.edu/audio/oralhist/<?php echo metadata('item', array('Item Type Metadata', 'Interview Media Identifier')); ?>"><?php echo metadata('item', array('Item Type Metadata', 'Interview Media Identifier')); ?></a></a></td></tr></table></div>
 <?php endif; ?>
 <?php if ($inlink = metadata('item', array('Item Type Metadata', 'Interview Linked Resource'), array('delimiter'=>', '))): ?>
-<?php foreach ($inlink as $linkval): ?>
-  <?php $parts = explode("|", $linkval): ?>
-  <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interview Linked Resource: </h4></td><td style="width:300px;text-align:left;">
-  <?php echo $parts[0]; ?>: <a href="<?php echo $parts[1]; ?>"><?php echo $parts[1]; ?></a></td></tr></table></div>
+<?php 
+foreach ($inlink as $linkval){
+$parts = explode("|", $linkval); ?>
+<div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interview Linked Resource: </h4></td><td style="width:300px;text-align:left;">
+<?php echo $parts[0]; ?>: <a href="<?php echo $parts[1]; ?>"><?php echo $parts[1]; ?></a></td></tr></table></div>
 <?php endforeach; ?>
 <?php endif; ?>
 
