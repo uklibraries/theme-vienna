a0c658212a690d4ea1724142fff2433327341013
libmanuk <eweig@email.uky.edu>
2016-05-01 17:04:10 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -202,7 +202,7 @@ if ('interviews' === $type): ?>
 <?php $urllink = metadata('item', array('Item Type Metadata', 'Interview Linked Resource');
 $parts = explode("|", $urllink);
 foreach($parts as $value) {
-$value= <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interview Linked Resource: </h4></td><td style="width:300px;text-align:left;">
+$value = <div class="item-description-static"><table style="width:100%;text-align:left;"><tr><td style="width:100px;text-align:left;"><h4>Interview Linked Resource: </h4></td><td style="width:300px;text-align:left;">
 echo $value;
 }
 ?>
