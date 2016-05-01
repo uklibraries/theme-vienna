e5561dc27a3785734fc12020307fa0b81cbed231
libmanuk <eweig@email.uky.edu>
2016-05-01 18:01:11 -0400

    Update show.php

--- items/show.php
+++ items/show.php
@@ -202,7 +202,7 @@ if ('interviews' === $type): ?>
 <?php $urllink = metadata('item', array('Item Type Metadata', 'Interview Linked Resource'));
 $parts = explode("|", $urllink);
 foreach($parts as &$value) {
-$value = <?php echo $parts[1]; ?><?php echo $parts[1]; ?>;
+$value = $parts[0] & $parts[1];
 echo $value;
 }
 ?>
